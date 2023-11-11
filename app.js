79f84856c948428fa8fab651633d71370f8fe53b
nohsiwon <98876@naver.com>
2023-11-12 01:31:46 +0900

    test2

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ const jwt = require('jsonwebtoken');
 
 const token = jwt.sign({ myPayloadData: 1234 }, 'mysecretkey');
 console.log(token); // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJteVBheWxvYWREYXRhIjoxMjM0LCJpYXQiOjE2Njc1NjE0NDB9.nvYSsLsT8jp7IfkbB2seCNeuLqRBgrrzDjKRFXjvoUE
+// test
